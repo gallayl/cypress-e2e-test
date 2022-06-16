1d38747f7328dc5da1f46c7e446ffee71e2740a7
Gallay Lajos <gallay.lajos@gmail.com>
2022-06-16 16:11:37 +0200

    field test restructure

--- cypress/e2e/02-fields.cy.ts
+++ cypress/e2e/02-fields.cy.ts
@@ -9,8 +9,28 @@ describe('Simple fields', () => {
   });
 
   describe('Validation', () => {
-    it('Should display the Required Field message for a missing select box');
+    // TODO: The validation should be tested for each field
+
+    describe('Pets', () => {
+      it('Should display an error message if no pet is selected');
+      it('Should...');
+    });
+
+    describe('Breakfast', () => {
+      it('Should display an error message if option is selected');
+      it('Should allow one option');
+      it('Should allow multiple options');
+    });
+
+    describe('Birth date', () => {
+      it('Should display an error message if birthdate is provided');
+      it('Should display an error message for wrong format');
+      it('Should accept a valid date');
+    });
   });
 
-  describe('The Happy path');
+  describe('The Happy path', () => {
+    it('Should proceed to the next step when all required fields are filled');
+    // TODO: The section should be submitted with valid values
+  });
 });
